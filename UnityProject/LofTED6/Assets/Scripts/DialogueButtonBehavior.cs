35eaac479a97659836ad516012e63b4c035b5b57
BarBenzvi <bar_bar@live.com>
2022-08-20 14:32:36 -0700

    Fixed some dialogue bugs

--- UnityProject/LofTED6/Assets/Scripts/DialogueButtonBehavior.cs
+++ UnityProject/LofTED6/Assets/Scripts/DialogueButtonBehavior.cs
@@ -6,12 +6,16 @@ public class DialogueButtonBehavior : MonoBehaviour
 {
     public GameObject NextDialogue = null;
     public DialogueBox DialogueOwner = null;
+    public bool ForceUnlockPlayer = false;
+    public bool SpawnOnPlayer = false;
 
     public void TriggerOption()
     {
         if(DialogueOwner != null)
         {
             DialogueOwner.NextDialogue = NextDialogue;
+            DialogueOwner.ForceUnlockPlayer = ForceUnlockPlayer;
+            DialogueOwner.SpawnOnPlayer = SpawnOnPlayer;
             DialogueOwner.FinishDialogue();
         }
     }
